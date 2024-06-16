33f954e6057a32f59337ed4221c66eca7b301114
R0YKER <152964692+R0YKER@users.noreply.github.com>
2024-06-16 15:00:30 +0300

    main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def task1(my_list):
 
     return my_list, min_element, max_element, list_length
 
-
 def task2(A, B, C):
     total_cost=sum([quantity * price for quantity, price in zip(B, C)])
     average_price=sum(C)/len(C)
@@ -21,13 +20,10 @@ def task2(A, B, C):
 
     return total_cost, average_price, most_stocked_item
 
-
 def task3():
     A1=[i for i in range(1, 26)]
     A2=[i for i in range (-25, 0)]
-
     return A1, A2
-
 
 def task4(my_string):
     count_a= my_string.count('a')
@@ -37,7 +33,6 @@ def task5(my_string):
     modified_str= my_string.replace("GOOD", "NICE")
     word_count= len(my_string.split())
     return modified_str, word_count
-
 def task6():
     list=[1,2,3,4,5]
     total_sum=sum(list)
